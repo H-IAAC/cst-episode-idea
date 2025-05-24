8f600c13b51dbc58fd2b427f3b7e1b530fe8f038
Bruno Silva <bruno_gsilva@outlook.com>
2025-05-24 16:39:48 -0300

    Improve Episode visualize_episodes

--- visualize_episodes.py
+++ visualize_episodes.py
@@ -1,8 +1,8 @@
+import argparse
 import json
 import matplotlib.pyplot as plt
 
-colors = ['b','g','r','c','m','y','k']
-
+colors = ['#e41a1c','#377eb8','#4daf4a','#984ea3','#ff7f00','#ffff33','#a65628','#f781bf','#999999']
 
 def get_pos(situation):
     for property in situation['l'][0]['l']:
@@ -15,20 +15,39 @@ def get_pos(situation):
             return float(x),float(y)
     return 0,0
 
-with open("SimpleEpisodes") as file:
-    idea = json.load(file)
+def visualize_episode(file_path, show_intermediate):
+    with open(file_path) as file:
+        idea = json.load(file)
 
-for i, episode in enumerate(idea['l']):
-    initial_sit, final_sit = None, None
-    for situation in episode['l']:
-        if "Initial" in situation['name']:
-            initial_sit = situation
-        elif  "Final" in situation['name']:
-            final_sit = situation
+    for i, episode in enumerate(idea['l']):
+        if show_intermediate:
+            px, py = None, None
+            for situation in episode['l']:
+                x, y = get_pos(situation)
+                if px and py:
+                    plt.arrow(px,py,(x-px),(y-py), color=colors[i % len(colors)], head_width=5, head_length=2)
+                px, py = x, y
+        else:
+            initial_sit, final_sit = None, None
+            for situation in episode['l']:
+                if "Initial" in situation['name']:
+                    initial_sit = situation
+                elif "Final" in situation['name']:
+                    final_sit = situation
 
-    if initial_sit and final_sit:
-        x1, y1 = get_pos(initial_sit)
-        x2, y2 = get_pos(final_sit)
+            if initial_sit and final_sit:
+                x1, y1 = get_pos(initial_sit)
+                x2, y2 = get_pos(final_sit)
+                plt.arrow(x1,y1,(x2 - x1),(y2 - y1), color=colors[i % len(colors)], head_width=5, head_length=2)
 
-        plt.arrow(x1,y2,(x2-x1),(y2-y1), color=colors[i], head_width=5, head_length=2)
-plt.show()
+    plt.show()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='visualize_episodes.py')
+
+    parser.add_argument('file_path')
+    parser.add_argument('--show-intermediate' , '-si', action='store_true')
+
+    args = parser.parse_args()
+
+    visualize_episode(args.file_path, args.show_intermediate)
